f53468271b6b1ee4bab3684dfd87bf3e077d627c
Damir <vanshot2020@bk.ru>
2024-11-13 03:09:15 +0300

    hw01 done #2
    Signed-off-by: Damir <vanshot2020@bk.ru>

--- hw01/src/hw01.cpp
+++ hw01/src/hw01.cpp
@@ -7,17 +7,20 @@ bool isPrime(int num) {
     }
     return true;
 }
+
 int hw01(int start, int end)
 {
     if(start < 10) return -1;
     int sum = 0;
+    bool foundPrime = false;
     for(int i = start;i<=end;i++) {
         if(isPrime(i)) {
+            foundPrime = true;
             int firstSymb = i/10;
             int secondSymb = i%10;
             int sumOfSymbs = firstSymb + secondSymb;
             sum+=sumOfSymbs;
         }
     }
-    return sum;
+    return foundPrime ? sum:-1;
 }
