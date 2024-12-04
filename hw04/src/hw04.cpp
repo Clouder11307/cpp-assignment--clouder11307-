b7d4614b078f43290d038cea654f3a99119ffefe
Damir <vanshot2020@bk.ru>
2024-12-04 21:32:10 +0300

    hw05 about the palindrome and return the strings unchanged

--- hw04/src/hw04.cpp
+++ hw04/src/hw04.cpp
@@ -12,13 +12,14 @@ std::size_t hw04(const int* input_array, std::size_t elem_count)
     std::size_t sum = 0;
     std::size_t size = 1;
 
-    int* out_data = new int[size];
+    int* out_data = new int[size]; // указатель на новую ячейку память
     out_data[0] = 0;
 
-    for(int i = 0;i<elem_count;i++) {
+    for(int i = 0;i < elem_count;i++) {
             if(input_array[i] < 0) {
             break;
         }
+
         int* new_data = new int[size + 1];
         copyArr(new_data,out_data,size);
         new_data[size] = input_array[size];
